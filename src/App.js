0b913c2d987ab9a09f0b4fa53d303158859280a3
supperjessecha <jesse@superblock.co>
2022-06-23 20:10:37 +0900

    add ifps functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
+import { create } from 'ipfs-http-client'
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
 function App() {
+  const [description, setDescription] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [name, setName] = useState();
+  const [resutlUrl, setResultUrl] = useState();
+
+
+  const callback =  async () =>{
+      const added = await client.add(JSON.stringify({"description":"LearningMan NFT.","image":"https://www.learningman.co/static/cb9efbeacb1737c460cc47aeeb70535e/98540/jesse-desc.png","name":"JESSE","attributes":[]}) )
+      
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      console.log(url)
+      setResultUrl(url)
+     }
+  
+  useEffect(()=>{
+     ( async () =>{
+      const added = await client.add(JSON.stringify({"description":"LearningMan NFT.","image":"https://www.learningman.co/static/cb9efbeacb1737c460cc47aeeb70535e/98540/jesse-desc.png","name":"JESSE","attributes":[]}) )
+      console.log(added)  
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      console.log(url)
+     })()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS NFT uploader</h1>
+      <div>
+        description: {description}
+      </div>
+      <div>
+        imageUrl: {imageUrl}
+      </div>
+      <div>
+        name: {name}
+      </div>
+      <div>
+        <label>
+          Description
+        </label>
+        <input type="text" onChange ={(e)=>{
+          setDescription(e.target.value)
+        }}  />
+      </div>
+       <div>
+        <label>
+          ImageUrl
+        </label>
+        <input type="text" onChange ={(e)=>{
+          setImageUrl(e.target.value)
+        }}  />
+      </div>
+       <div>
+        <label>
+          Name
+        </label>
+        <input type="text" onChange ={(e)=>{
+          setName(e.target.value)
+        }}  />
+      </div>
+      <button onClick={()=>{callback()}}>
+        send
+      </button>
+
+
+      <div>
+        result IPFS URL : {resutlUrl}
+      </div>
+    
     </div>
   );
 }
 
-export default App;
+export default App
